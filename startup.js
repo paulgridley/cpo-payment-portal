bcc811b48c9e021b0b8760d2890fe8d2b6060dd8
paul <paul.gridley@mail.com>
2025-09-12 12:56:37 +0100

    delete all files

--- startup.js
+++
@@ -1,9 +0,0 @@
-// Azure App Service startup file
-// This ensures the application starts correctly on Azure Linux App Service
-const path = require('path');
-
-// Set the main application file
-process.chdir(__dirname);
-
-// Start the application
-require('./dist/index.js');
